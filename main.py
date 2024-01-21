aaa1b7f4ece98d6654367c40553481fe94e6500e
beniboymael <benimoleri@gmail.com>
2024-01-21 17:51:21 -0500

    Assignment

---
+++ main.py
@@ -0,0 +1,4 @@
+print('Ben Meal he/him')
+print("I'm taking this class because it is required for the cybersecurity degree")
+print("When not in class I am usually at work")
+print('I play the guitar and the piano')
